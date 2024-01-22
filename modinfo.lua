86fdd50611c740585c8fe4102db8c2aa78f88398
genchsusu <pctxiaozhu@gmail.com>
2024-01-22 21:29:34 +0800

    feat: fix locale bug

--- modinfo.lua
+++ modinfo.lua
@@ -5,7 +5,7 @@ end
 name = 'No Spacing'
 name = e_or_z("No Spacing", "建造无间距")
 author = 'OpenSource'
-version = '1.0.1'
+version = '1.0.2'
 description = e_or_z(
     [[
 Deploy all things with no spacing. 
@@ -34,24 +34,24 @@ all_clients_require_mod = true
 
 priority = 99
 
-local function AddConfig(label_en, label_zh, name, hover_en, hover_zh, options, default)
+local function AddConfig(label, name, hover, options, default)
 	return {
-		label = e_or_z(label_en, label_zh),
+		label = label,
 		name = name,
-		hover = e_or_z(hover_en, hover_zh) or '',
+		hover = hover or '',
 		options = options or {
             {description = e_or_z('No spacing', '无间距'), data = 0, hover = e_or_z('No spacing', '没有间距')},
 			{description = e_or_z('Small', '小间距'), data = 1, hover = e_or_z('Small spacing', '有较小间距')},
 			{description = e_or_z('Default', '默认'), data = false, hover = e_or_z('Default spacing', '默认间距')}
 		},
-		default = default or 0
+		default = default or true
 	}
 end
 
 configuration_options =
 {
-    AddConfig(e_or_z("Building", "建造类"), "build", e_or_z("", ""), nil, nil),
-    AddConfig(e_or_z("Deploy/Planting", "放置/种植类"), "deploy", e_or_z("", ""), nil, nil),
+    AddConfig(e_or_z("Building", "建造类"), "build"),
+    AddConfig(e_or_z("Deploy and Planting", "放置 & 种植类"), "deploy"),
     AddConfig(
         e_or_z("Specific Buildings", "部分建筑"),
         "GoThrough",
